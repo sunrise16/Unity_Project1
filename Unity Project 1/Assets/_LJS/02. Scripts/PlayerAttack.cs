3da7aa753b64f86a886c1dc51c1f957a0861cf82
Lee Jong Seok <sunrise16@naver.com>
2020-06-01 14:20:02 +0900

    유니티 프로젝트 생성

---
+++ Unity Project 1/Assets/_LJS/02. Scripts/PlayerAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private GameObject PlayerBullet1;
+    private float createTime = 0.1f;
+    private float currentCreateTime = 0.09f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            currentCreateTime += Time.deltaTime;
+            if (currentCreateTime > createTime)
+            {
+                Instantiate(PlayerBullet1, transform.position, Quaternion.LookRotation(transform.forward));
+                currentCreateTime = 0.0f;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            currentCreateTime = 0.09f;
+        }
+    }
+}
---
+++ Unity Project 1/Assets/_LJS/02. Scripts/PlayerMove.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // 플레이어 이동
+    public float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 저속 이동
+        if (Input.GetKey(KeyCode.LeftShift)) { speed = 2.0f; }
+        else { speed = 5.0f; }
+
+        // 이동 함수
+        Move();
+
+        // 화면 밖으로 벗어나지 못하게 하기
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+
+        if (pos.x < 0.05f) { pos.x = 0.05f; }
+        if (pos.x > 0.95f) { pos.x = 0.95f; }
+        if (pos.y < 0.025f) { pos.y = 0.025f; }
+        if (pos.y > 0.975f) { pos.y = 0.975f; }
+
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(h, v, 0);
+
+        // 이동 공식
+        // 1)
+        // transform.Translate(h * speed * Time.deltaTime, v * speed * Time.deltaTime, 0);
+        // 2)
+        // Vector3 dir = Vector3.right * h + Vector3.up * v;
+        // 3)
+        transform.Translate(dir * speed * Time.deltaTime);
+        // 4)
+        // transform.position += (dir * speed * Time.deltaTime);
+    }
+}
